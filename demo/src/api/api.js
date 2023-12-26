5f5227d3a3f3644a0ab897a47f8322efc90873eb
David Pittman <53283971+dopittman@users.noreply.github.com>
2023-12-26 18:10:19 -0500

    feat: add express routing

--- demo/src/api/api.js
+++ demo/src/api/api.js
@@ -1,7 +1,12 @@
-module.exports.handler = (event, context, done) => {
-	done(null, {
-		statusCode: 200,
-		headers: {},
-		body: JSON.stringify({ message: "ABDEF" }),
+const express = require("express");
+const http = require("serverless-http");
+
+const app = express();
+
+app.get("/", (req, res) => {
+	res.json({
+		message: "in the api.js file",
 	});
-};
+});
+
+module.exports.handler = http(app);
