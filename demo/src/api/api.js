e66e0c872e764878629e490f69c5bd6e2da1c2d7
David Pittman <53283971+dopittman@users.noreply.github.com>
2023-12-26 18:17:36 -0500

    feat: update routes

--- demo/src/api/api.js
+++ demo/src/api/api.js
@@ -4,9 +4,19 @@ const http = require("serverless-http");
 const app = express();
 
 app.get("/", (req, res) => {
-	res.json({
-		message: "in the api.js file",
-	});
+	res.json({ message: "ok" });
+});
+
+app.get("/todos", (req, res) => {
+	const todos = [
+		{ id: 1, name: "clean up", status: "open" },
+		{ id: 1, name: "cook", status: "done" },
+	];
+	res.json({ data: todos });
+});
+
+app.get("/todos/:todo", (req, res) => {
+	res.json({ id: 1, name: "clean up", status: "open" });
 });
 
 module.exports.handler = http(app);
